41de380d0ad51c63d9467a5b08aa98ace580c0fc
jimmy_olsen <102313483+JimmyOlsen05@users.noreply.github.com>
2024-06-02 18:39:08 +0000

    app.py

--- app.py
+++ app.py
@@ -75,21 +75,12 @@ def home():
         return
 
     # Render the background image
-    st.markdown(
-        f"""
-        <style>
-        .stApp {{
-            background-image: url("data:image/png;base64,{background_image_path}");
-            background-size: cover;
-        }}
-        </style>
-        """,
-        unsafe_allow_html=True
-    )
+    st.image(background_image_path, use_column_width=True)
 
     # Render content on top of the background image
     st.title("Home Page")
     st.write("Welcome to the Autism Prediction Ensemble Model application!")
+
 
 def predict():
     st.title("Autism Prediction Ensemble Model")
