addab56637d1f9241ccf168b56aaf80b628b250a
jimmy_olsen <102313483+JimmyOlsen05@users.noreply.github.com>
2024-06-05 20:58:49 +0000

    Update app.py

--- app.py
+++ app.py
@@ -148,7 +148,7 @@ def predict():
         
         st.markdown(
         """
-        <div style="background-color: #A27F0B; padding: 10px;">
+        <div style="background-color: #FA3127; padding: 10px;">
         Select '1' or '0' from the dropdowns
         
         i.e Where 1 = Always, Usually or Sometimes and 0 = Rarely or Never
